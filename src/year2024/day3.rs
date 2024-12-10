ab1ced9ff0ed8596a60aa02c009fb96c12726a06
bhollier <b.hollier123@gmail.com>
2024-12-10 20:12:58 +0000

    day 3 pt 2

--- src/year2024/day3.rs
+++ src/year2024/day3.rs
@@ -1,11 +1,40 @@
 //! https://adventofcode.com/2024/day/3
 
+use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashSet;
+use std::ops::{Not, Range};
+
+lazy_static! {
+    static ref MUL_REGEX: Regex = Regex::new(r"mul\((\d\d?\d?),(\d\d?\d?)\)").unwrap();
+}
 
 fn part_1(input: &String) -> i64 {
-    let regex = Regex::new(r"mul\((\d\d?\d?),(\d\d?\d?)\)").unwrap();
-    regex
+    MUL_REGEX
         .captures_iter(input.as_str())
+        .map(|g| g.extract())
+        .map(|(_, [left, right])| (left.parse::<i64>().unwrap(), right.parse::<i64>().unwrap()))
+        .map(|(left, right)| left * right)
+        .sum()
+}
+
+fn part_2(input: &String) -> i64 {
+    // Construct a set of ranges between `don't()` and `do()` where matches should be ignored
+    let disabled_regex = Regex::new(r"(don't\(\))[\s\S]+?(do\(\))").unwrap();
+    let disabled_ranges: HashSet<Range<usize>> = HashSet::from_iter(
+        disabled_regex
+            .captures_iter(input.as_str())
+            .map(|g| g.get(0).unwrap().range()),
+    );
+    MUL_REGEX
+        .captures_iter(input.as_str())
+        .filter(|g| {
+            let pos = &g.get(0).unwrap().start();
+            disabled_ranges
+                .iter()
+                .any(|range| range.contains(pos))
+                .not()
+        })
         .map(|g| g.extract())
         .map(|(_, [left, right])| (left.parse::<i64>().unwrap(), right.parse::<i64>().unwrap()))
         .map(|(left, right)| left * right)
@@ -14,4 +43,5 @@ fn part_1(input: &String) -> i64 {
 
 pub fn run(input: &String) {
     println!("part 1 solution: {}", part_1(input));
+    println!("part 2 solution: {}", part_2(input));
 }
